26d769e8b8d2a24ac139b496f14459ccf298c0be
Bhaskar Jayaraman <bjayaraman@Bhaskars-MacBook-Pro.local>
2020-10-06 21:03:22 -0700

    New terraform

---
+++ first_terraform.tf
@@ -0,0 +1,35 @@
+# Configure the Fastly Provider
+provider "fastly" {
+}
+
+# output varialbles on the console
+output "Fastly-Version" {
+  value = "${fastly_service_v1.second_terraform_service.active_version}"
+}
+
+# Create a Service
+
+resource "fastly_service_v1" "redirect_chase_terraform_service" {
+  name = "Second Fastly Terraform Service"
+
+  domain {
+    name    = "abcd.redirect.com"
+    comment = "Redirect Chase Terraform demo"
+  }
+
+  backend {
+    address = "151.101.2.133"
+    name    = "d.sni.global.fastly.net"
+    port    = 443
+  }
+
+ snippet {
+   name     = "Change_jpg_ttl"
+   type     = "fetch"
+   priority = 8
+   content = "if ( req.url ~ \"\\.(jpeg|jpg|gif)$\" ) {\n # jpeg/gif TTL\n set beresp.ttl = 172800s;\n }\n set beresp.http.Cache-Control = \"max-age=\" beresp.ttl;"
+ }
+
+  force_destroy = true
+}
+

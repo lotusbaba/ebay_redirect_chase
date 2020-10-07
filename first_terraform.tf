4531457b0f0ecccdab54740f1c077754f9174fa5
Bhaskar Jayaraman <bjayaraman@Bhaskars-MacBook-Pro.local>
2020-10-06 23:14:44 -0700

    Added redirect chase code

--- first_terraform.tf
+++ first_terraform.tf
@@ -18,24 +18,71 @@ resource "fastly_service_v1" "redirect_chase_terraform_service" {
   }
 
   backend {
-    address = "151.101.2.133"
-    name    = "d.sni.global.fastly.net"
+    address = "run.mocky.io"
+    name    = "From fiddle b15757c0"
     port    = 443
   }
 
  snippet {
-   name     = "Change_jpg_ttl"
-   type     = "fetch"
+   name     = "Force clustering"
+   type     = "recv"
    priority = 8
-   content = "if ( req.url ~ \"\\.(jpeg|jpg|gif)$\" ) {\n # jpeg/gif TTL\n set beresp.ttl = 172800s;\n }\n set beresp.http.Cache-Control = \"max-age=\" beresp.ttl;"
+   content = "if (req.restarts == 0) {	\n unset req.http.redirectchase_restart; \n } \n # If restarting for a redirect, re-enable clustering \n if (req.http.redirectchase_restart) { \n set req.http.Fastly-Force-Shield = \"1\"; \n }"
 }
 
 snippet {
-   name     = "Override_host"
-   type     = "recv"
+   name     = "Unset_header_miss"
+   type     = "miss"
    priority = 8
-   content = "set req.http.host = \"deciduous-impossible-expansion.glitch.me\";"
+   content = "unset bereq.http.redirectchase_restart;"
  }
+
+snippet {
+   name     = "Unset_header_pass"
+   type     = "pass"
+   priority = 8
+   content = "unset bereq.http.redirectchase_restart;"
+ }
+
+snippet	{
+   name     = "Track response"
+   type     = "fetch"
+   priority = 8
+   content = " # Tag the response so that we can track whether it came from a\n # customer origin (and not a Fastly shield POP) \n set beresp.http.redirectchase_isorigin = req.backend.is_origin;
+}
+
+snippet {
+  name	= "Redirect chase"
+  type	= "deliver"
+  priority = 8
+  content = "declare local var.maxRedirects INTEGER;
+declare local var.curRedirects INTEGER;
+declare local var.redirectPath STRING;
+declare local var.redirectHost STRING;
+
+set var.maxRedirects = 2;
+set var.curRedirects = std.atoi(if (req.http.redirectchase_restart, req.http.redirectchase_restart, \"0\"));
+
+# Perform an internal redirect if...
+if (
+  resp.status >= 300 && resp.status < 400 &&            # the response is a redirect
+  resp.http.redirectchase_isorigin &&                   # and it came from a customer origin
+  var.curRedirects < var.maxRedirects &&                # and we haven't exceeded a maximum number of redirects
+  resp.http.location ~ \"^(?:https?://([^/]+))?(/.*)?$\") # and there's a valid location header
+{
+  set var.redirectHost = re.group.1;
+  set var.redirectPath = re.group.2;
+  
+  # Only do so if the location header does not specify a host, or the host matches the client-side host header, or a whitelist of 'local' domains
+  if (var.redirectHost == \"\" || var.redirectHost == req.http.host) {
+    set req.url = if (var.redirectPath, var.redirectPath, \"/\");
+    set var.curRedirects += 1;
+    set req.http.redirectchase_restart = var.curRedirects;
+    restart;
+  }
+}
+unset resp.http.redirectchase_isorigin;"
+}
 
   force_destroy = true
 }
